0c737f26d92facd0f97d70303823ec0cf2e95ec6
rodrigo-llanos <rodrigo.llanos@vodafone.com>
2023-06-16 08:41:02 +0200

    refactor: extract email validation to a utils

--- src/modules/payment/domain/PaymentTo.ts
+++ src/modules/payment/domain/PaymentTo.ts
@@ -1,12 +1,13 @@
+import { validateEmail } from '../../shared/application/validateEmail';
 import { PaymentToError } from './errors/PaymentToError';
 
 export class PaymentTo {
   private readonly _value: string;
 
   constructor(to: string) {
-    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    const emailIsValid = validateEmail(to);
 
-    if (!emailRegex.test(to)) {
+    if (!emailIsValid) {
       throw new PaymentToError();
     }
 
